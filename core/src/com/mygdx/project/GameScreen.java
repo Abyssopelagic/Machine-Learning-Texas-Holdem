927f76f82e887965f5d9d614a6875477c72769b2
Keenan <ksbaynard@hotmail.com>
2017-01-14 20:08:39 -0600

    Working Statistical Computer Play

--- core/src/com/mygdx/project/GameScreen.java
+++ core/src/com/mygdx/project/GameScreen.java
@@ -184,6 +184,7 @@ public class GameScreen extends ApplicationAdapter {
                     handTies = false;
                     playerWins = false;
                     computerWins = false;
+                    //lolz
                 }
             }
         });
